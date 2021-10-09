511a15bd0587378d6be2c651f4d22c23b313dc8d
chimpaji <chimpaji@outlook.com>
2021-10-09 13:07:47 +0700

    done

--- slice.go
+++ slice.go
@@ -2,9 +2,49 @@ package main
 
 import "fmt"
 
+type Product struct {
+	title string
+	id string
+	price int
+}
+
 func main() {
+	//#1 origin slice
+	//create string slice without define length
 	hobbies := []string{"football", "basketball", "coding"}
-	fmt.Print(hobbies)
+
+	//#2   
+	//print all data
+	fmt.Println(hobbies)
+	//print only the first one
+	fmt.Println(hobbies[0])
+	//print the secound and third
+	fmt.Println(hobbies[1:])
+	
+	//#3
+	//create a slice that contain 1st and 2nd elements
+	littleHobbies := hobbies[:2]
+	anotherLittleHobbies := [2]string{}
+	anotherLittleHobbies[0] = hobbies[0]
+	anotherLittleHobbies[1] = hobbies[1]
+	fmt.Println(littleHobbies)
+	fmt.Println(anotherLittleHobbies)
+
+	//#4
+	//Reslice #3 to contain secound and the last element from origin
+	littleHobbies = littleHobbies[1:3]  //it will go back to origin slice of littleHobbies and go further to 3rd element
+	fmt.Println(littleHobbies)
+
+	//#5 create goal slice
+	courseGoal := []string{"own 100 bitcoins","own 100 eth"}  
+
+	//#6
+	courseGoal = append(courseGoal,"own 100 sol")
+	fmt.Println(courseGoal)
+
+	productList := [] Product {{"New product1","new-prod-1",99},{"New product2","new-prod-2",199}}
+	productList = append(productList,Product{"New product3","new-prod-3",1999})
+	fmt.Println(productList)
 }
 
 // Time to practice what you learned!
